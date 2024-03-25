92494cc84f0afc3a1f0246a93d1b65b5be3f5b7b
Michael Farquhar <michaelfarquhar4@gmail.com>
2024-03-25 17:03:22 -0400

    Integrate @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
+
+import { SITE_URL } from './src/consts';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown()]
+  site: SITE_URL,
+  integrations: [tailwind(), partytown(), sitemap()]
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,6 @@
 // Affected Files: Footer.astro | Header.astro | BaseHead.astro
 export const SITE_TITLE = 'Astro Blueprint';
 export const SITE_DESCRIPTION = 'This is the description.';
+
+// Affected Files: astro.config.mjs | robots.txt.ts
+export const SITE_URL = 'http://localhost:4321';
