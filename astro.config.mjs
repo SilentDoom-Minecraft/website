93cd39fcd9978d404a1710771a0167b84818026b
Michael Farquhar <michaelfarquhar4@gmail.com>
2024-03-27 09:34:08 -0400

    Add astro-icon and replace header logo with Icon component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
-
 import sitemap from "@astrojs/sitemap";
-
 import { SITE_URL } from './src/consts';
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [tailwind(), partytown(), sitemap()]
+  integrations: [tailwind(), partytown(), sitemap(), icon()]
 });
