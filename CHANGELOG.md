c8a73dae2193245b88a8e1258b01ab4d35f12830
Michael Farquhar <michaelfarquhar4@gmail.com>
2024-04-06 14:44:33 -0400

    Improve meta tags and add astro:transitions.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 1.0.1 / 2024-03-26
+
+-   Improved meta tags
+-   Added variable meta title and description for each page
+
 # 1.0.0 / 2024-03-26
 
 -   Initial release
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
-import sitemap from "@astrojs/sitemap";
-import { SITE_URL } from './src/consts';
+import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
-import icon from "astro-icon";
+import { SITE_URL } from './src/consts';
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE_URL,
-  integrations: [tailwind(), partytown(), sitemap(), icon()]
+    site: SITE_URL,
+    integrations: [tailwind(), partytown(), sitemap(), icon()],
 });
