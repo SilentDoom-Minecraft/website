13bc36cf48963466f11860694e3bacecc09f6e0c
Michael Farquhar <michaelfarquhar4@gmail.com>
2024-05-16 22:45:24 -0400

    Add site domain

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,4 @@ export const SITE_TITLE = 'SilentDoom MC';
 export const SITE_DESCRIPTION = 'Mods, Datapacks and Modpacks created by SilentDoom.';
 
 // Affected Files: astro.config.mjs | robots.txt.ts
-export const SITE_URL = 'http://localhost:4321';
+export const SITE_URL = 'https://silentdoommc.netlify.app/';
