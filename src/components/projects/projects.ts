76b7903a08765fb23e58737984f115a15c19f81b
Michael Farquhar <michaelfarquhar4@gmail.com>
2024-05-16 21:51:15 -0400

    Add projects section and data

---
+++ src/components/projects/projects.ts
@@ -0,0 +1,83 @@
+export const projects = {
+    datapacks: [
+        {
+            title: 'Villagers Drop Emeralds on Death',
+            description:
+                'Villagers and wandering traders drop a single emerald on death. Inspired by the mod of the same name, but as a minecraft datapack.',
+            image: 'villagers-drop-emeralds.webp',
+            tags: [''],
+            curseforge:
+                'https://www.curseforge.com/minecraft/texture-packs/villagers-drop-emeralds-on-death-datapack',
+            modrinth: 'https://modrinth.com/datapack/villagers-drop-emerald-on-death',
+        },
+        {
+            title: 'Disable Advancements',
+            description: 'Minecraft datapack which disables all vanilla advancements.',
+            image: 'disable-advancements.webp',
+            tags: [''],
+            curseforge:
+                'https://www.curseforge.com/minecraft/texture-packs/disable-advancements-datapack',
+            modrinth: 'https://modrinth.com/datapack/disable-advancements',
+        },
+        {
+            title: 'Grant All Recipes',
+            description: 'Automatically unlocks all recipes for all players.',
+            image: 'grant-all-recipes.webp',
+            tags: [''],
+            curseforge:
+                'https://www.curseforge.com/minecraft/texture-packs/grant-all-recipes-datapack',
+            modrinth: 'https://modrinth.com/datapack/grant-all-recipes-datapack',
+        },
+        {
+            title: 'Disable Advancements',
+            description: 'Minecraft datapack which disables all vanilla advancements.',
+            image: 'disable-advancements.webp',
+            tags: [''],
+            curseforge:
+                'https://www.curseforge.com/minecraft/texture-packs/disable-advancements-datapack',
+            modrinth: 'https://modrinth.com/datapack/disable-advancements',
+        },
+        {
+            title: 'Shortcut Recipes',
+            description:
+                'Adds secondary shortcut recipes to already pre-existing recipes.',
+            image: 'shortcut-recipes-datapack.webp',
+            tags: [''],
+            curseforge:
+                'https://www.curseforge.com/minecraft/texture-packs/shortcut-recipes-datapack',
+            modrinth: 'https://modrinth.com/datapack/shortcut-recipes',
+        },
+    ],
+    mods: [
+        {
+            title: 'Easy Commands',
+            description: 'A set of easy-to-use command aliases.',
+            image: 'easy-commands.webp',
+            tags: ['Forge', 'Fabric'],
+            curseforge:
+                'https://www.curseforge.com/minecraft/mc-mods/easy-commands-forge-fabric',
+            modrinth: 'https://modrinth.com/mod/easy-commands',
+        },
+        {
+            title: 'Shortcut Recipes',
+            description:
+                'Adds secondary shortcut recipes to already pre-existing recipes.',
+            image: 'shortcut-recipes.webp',
+            tags: ['Forge', 'Fabric'],
+            curseforge:
+                'https://www.curseforge.com/minecraft/mc-mods/shortcut-recipes-forge-fabric',
+            modrinth: 'https://modrinth.com/mod/shortcutrecipes',
+        },
+    ],
+    modpacks: [
+        {
+            title: 'Mocha Latte',
+            description:
+                'Mocha Latte is a Vanilla+ Forge Modpack designed to stay as true to vanilla as possible, with a primary focus on optimization and quality of life improvements.',
+            image: 'mocha-latte.webp',
+            tags: ['Forge', '1.20.1'],
+            curseforge: 'https://www.curseforge.com/minecraft/modpacks/mocha-latte',
+            modrinth: '',
+        },
+    ],
+};
